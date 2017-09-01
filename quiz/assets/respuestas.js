e44da1da9a6eef7c939da464e0e3c5bdb6b4253d
Héctor Mendoza <hecttormendoza@gmail.com>
2017-09-01 09:37:15 -0500

    Quiz

---
+++ quiz/assets/respuestas.js
@@ -0,0 +1,326 @@
+var dbQuiz = {
+    "hombre": [
+      {
+        "ans": ["piña", "chica", "clásico", "verdes"],
+        "models": ["Ramirez Caoba", "Lucas Humo", "Seymour Grafito", "Ginsberg Sombra"]
+      },
+      {
+        "ans": ["piña", "chica", "clásico", "rojos"],
+        "models": ["Ramirez Grafito", "Lucas ónix", "Seymour ónix", "Ginsberg caoba"]
+      },
+      {
+        "ans": ["piña", "chica", "moderno", "verdes"],
+        "models": ["Barragán Caoba", "Woody Grafito", "Seymour grafito", "Ginsberg cereza"]
+      },
+      {
+        "ans": ["piña", "chica", "moderno", "rojos"],
+        "models": ["Barragán Caoba", "Woody Grafito", "Seymour sombra", "Ginsberg sombra"]
+      },
+      {
+        "ans": ["piña", "grande", "clásico", "verdes"],
+        "models": ["Martin ónix", "Woody Grafito", "Stanley Nogal", "Truman Onix"]
+      },
+      {
+        "ans": ["piña", "grande", "clásico", "rojos"],
+        "models": ["Martin Caoba", "Woody Grafito", "Stanley Grafito", "Truman Onix"]
+      },
+      {
+        "ans": ["piña", "grande", "moderno", "verdes"],
+        "models": ["Martin Maple Burbuja", "Woody Grafito", "Wally ónix", "Truman Cedro matte"]
+      },
+      {
+        "ans": ["piña", "grande", "moderno", "rojos"],
+        "models": ["Martin Maple Burbuja", "Woody Grafito", "Wally Mocha", "Truman Cedro matte"]
+      },
+      {
+        "ans": ["sandía", "chica", "clásico", "verdes"],
+        "models": ["Ramirez Grafito", "Seymour Grafito", "Ginsberg Caoba", "Aldous Grafito"]
+      },
+      {
+        "ans": ["sandía", "chica", "clásico", "rojos"],
+        "models": ["Ramirez Caoba", "Seymour Sombra", "Ginsberg sombra", "Aldous Caoba"]
+      },
+      {
+        "ans": ["sandía", "chica", "moderno", "verdes"],
+        "models": ["Daria Jr. Musgo", "Leon Burbuja", "Seymour sombra", "Cooper canela"]
+      },
+      {
+        "ans": ["sandía", "chica", "moderno", "rojos"],
+        "models": ["Daria Jr. Niquel", "Leon Caoba", "Seymour sombra", "Cooper Ónix"]
+      },
+      {
+        "ans": ["sandía", "grande", "clásico", "verdes"],
+        "models": ["Esquivel Grafito", "Martin Ónix", "Truman Ónix", "Clark Burbuja"]
+      },
+      {
+        "ans": ["sandía", "grande", "clásico", "rojos"],
+        "models": ["Esquivel Niquel", "Martin Ónix", "Truman Ónix", "Clark Grafito"]
+      },
+      {
+        "ans": ["sandía", "grande", "moderno", "verdes"],
+        "models": ["Woody Grafito", "Moleman Sombra", "Esquivel Niquel", "Daria Burbuja"]
+      },
+      {
+        "ans": ["sandía", "grande", "moderno", "rojos"],
+        "models": ["Woody Cedro", "Moleman Onix", "Esquivel Niquel", "Daria Burbuja"]
+      },
+      {
+        "ans": ["melón", "chica", "clásico", "verdes"],
+        "models": ["Clark Grafito", "Walter Acero", "Lucas Humo", "Ramirez grafito"]
+      },
+      {
+        "ans": ["melón", "chica", "clásico", "rojos"],
+        "models": ["Clark Caoba", "Walter Acero", "Lucas Onix", "Ramirez grafito"]
+      },
+      {
+        "ans": ["melón", "chica", "moderno", "verdes"],
+        "models": ["Clark Burbuja", "Walter Onix", "Lucas Humo", "Seymour Sombra"]
+      },
+      {
+        "ans": ["melón", "chica", "moderno", "rojos"],
+        "models": ["Clark Caoba", "Walter Onix", "Lucas Caoba", "Seymour Grafito"]
+      },
+      {
+        "ans": ["melón", "grande", "clásico", "verdes"],
+        "models": ["Clark Grafito", "Esquivel Grafito", "Stanley Onix", "Martin Onix"]
+      },
+      {
+        "ans": ["melón", "grande", "clásico", "rojos"],
+        "models": ["Clark Caoba", "Esquivel Niquel", "Stanley Sombra", "Martin Onix"]
+      },
+      {
+        "ans": ["melón", "grande", "moderno", "verdes"],
+        "models": ["Clark Burbuja", "Janis Cosmico", "Martin Maple B.", "Woody Grafito"]
+      },
+      {
+        "ans": ["melón", "grande", "moderno", "rojos"],
+        "models": ["Clark Caoba", "Janis Cobalto", "Martin Onix", "Woody Grafito"]
+      },
+      {
+        "ans": ["calabaza", "chica", "clásico", "verdes"],
+        "models": ["Parks Plomo", "Aldous Grafito", "Ramirez Grafito", "Daria Jr. Onix"]
+      },
+      {
+        "ans": ["calabaza", "chica", "clásico", "rojos"],
+        "models": ["Parks Latón", "Aldous Nogal", "Ramirez Grafito", "Daría Jr. cobalto"]
+      },
+      {
+        "ans": ["calabaza", "chica", "moderno", "verdes"],
+        "models": ["Gonzo Latón", "Aldous Burubuja", "Daria Jr. Musgo", "Parks Latón"]
+      },
+      {
+        "ans": ["calabaza", "chica", "moderno", "rojos"],
+        "models": ["Gonzo Latón", "Aldous Cereza", "Daria Jr. Cobalto", "Parks Plomo"]
+      },
+      {
+        "ans": ["calabaza", "grande", "clásico", "verdes"],
+        "models": ["Daria Onix", "Moleman Sombra", "Aldous Grafito", "Gonzo Latón"]
+      },
+      {
+        "ans": ["calabaza", "grande", "clásico", "rojos"],
+        "models": ["Daria Onix", "Moleman Onix", "Aldous Nogal", "Gonzo Latón"]
+      },
+      {
+        "ans": ["calabaza", "grande", "moderno", "verdes"],
+        "models": ["Gonzo Latón", "Moleman Sombra", "Aldous Burubuja", "Daria Cereza"]
+      },
+      {
+        "ans": ["calabaza", "grande", "moderno", "rojos"],
+        "models": ["Gonzo Latón", "Moleman Caoba", "Aldous Cereza", "Daria Miel"]
+      },
+      {
+        "ans": ["guanábana", "chica", "clásico", "verdes"],
+        "models": ["Ginsberg Sombra", "Ramirez Latte", "Seymour ónix", "Truman cedro matte"]
+      },
+      {
+        "ans": ["guanábana", "chica", "clásico", "rojos"],
+        "models": ["Ginsberg Caoba", "Ramirez grafito", "Seymour Grafito", "Truman Ónix"]
+      },
+      {
+        "ans": ["guanábana", "chica", "moderno", "verdes"],
+        "models": ["Ginsberg Sombra", "Ramirez Latte", "Seymour ónix", "Truman cedro matte"]
+      },
+      {
+        "ans": ["guanábana", "chica", "moderno", "rojos"],
+        "models": ["Ginsberg Cereza", "Ramirez Caoba", "Seymour Sombra", "Truman Ónix"]
+      },
+      {
+        "ans": ["guanábana", "grande", "clásico", "verdes"],
+        "models": ["Janis Nogal", "Moleman Onix", "Truman cedro matte", "Stanley Nogal"]
+      },
+      {
+        "ans": ["guanábana", "grande", "clásico", "rojos"],
+        "models": ["Janis Niquel", "Moleman Caoba", "Truman Ónix", "Stanley Onix"]
+      },
+      {
+        "ans": ["guanábana", "grande", "moderno", "verdes"],
+        "models": ["Janis Niquel", "Moleman Sombra", "Truman cedro matte", "Gonzo Cobre"]
+      },
+      {
+        "ans": ["guanábana", "grande", "moderno", "rojos"],
+        "models": ["Janis Cósmico", "Moleman Caoba", "Truman Ónix", "Gonzo Latón"]
+      }
+    ],
+    "mujer" : [
+      {
+        "ans": ["piña", "chica", "clásico", "verdes"],
+        "models": ["Ramirez Caoba", "Chavela Sombra", "Seymour Grafito", "Ginsberg Sombra"]
+      },
+      {
+        "ans": ["piña", "chica", "clásico", "rojos"],
+        "models": ["Ramirez Grafito", "Chavela Sombra", "Seymour ónix", "Ginsberg caoba"]
+      },
+      {
+        "ans": ["piña", "chica", "moderno", "verdes"],
+        "models": ["Barragán Caoba", "Chavela Eucalipto", "Seymour grafito", "Ginsberg cereza"]
+      },
+      {
+        "ans": ["piña", "chica", "moderno", "rojos"],
+        "models": ["Barragán Caoba", "Chavela  Chicle", "Seymour sombra", "Ginsberg sombra"]
+      },
+      {
+        "ans": ["piña", "grande", "clásico", "verdes"],
+        "models": ["Martin ónix", "Woody Caoba", "Wally Onix", "Truman Cedro matte"]
+      },
+      {
+        "ans": ["piña", "grande", "clásico", "rojos"],
+        "models": ["Martin Maple", "Woody Cedro", "Wally Mocha", "Truman Onix"]
+      },
+      {
+        "ans": ["piña", "grande", "moderno", "verdes"],
+        "models": ["Martin Eucalipto", "Woody Caoba", "Wally ónix", "Truman Cedro matte"]
+      },
+      {
+        "ans": ["piña", "grande", "moderno", "rojos"],
+        "models": ["Martin Maple", "Woody Cedro", "Wally Mocha", "Truman Onix"]
+      },
+      {
+        "ans": ["sandía", "chica", "clásico", "verdes"],
+        "models": ["Ramirez Latte", "Seymour Ónix", "Ginsberg Caoba", "Aldous Nogal"]
+      },
+      {
+        "ans": ["sandía", "chica", "clásico", "rojos"],
+        "models": ["Ramirez Caoba", "Seymour Sombra", "Ginsberg sombra", "Aldous Caoba"]
+      },
+      {
+        "ans": ["sandía", "chica", "moderno", "verdes"],
+        "models": ["Parks Laton", "Chavela Eucalipto", "Seymour sombra", "Barragán Chicle"]
+      },
+      {
+        "ans": ["sandía", "chica", "moderno", "rojos"],
+        "models": ["Parks Cobre", "Chavela Chicle", "Seymour sombra", "Barragán Eucalipto"]
+      },
+      {
+        "ans": ["sandía", "grande", "clásico", "verdes"],
+        "models": ["Esquivel Eucalipto", "Martin Maple", "Truman sombra", "Moleman Caoba"]
+      },
+      {
+        "ans": ["sandía", "grande", "clásico", "rojos"],
+        "models": ["Esquivel Niquel", "Martin Ónix", "Truman Ónix", "Moleman sombra"]
+      },
+      {
+        "ans": ["sandía", "grande", "moderno", "verdes"],
+        "models": ["Woody Grafito", "Moleman Sombra", "Esquivel Niquel", "Daria Cereza"]
+      },
+      {
+        "ans": ["sandía", "grande", "moderno", "rojos"],
+        "models": ["Woody Cedro", "Moleman Onix", "Esquivel Niquel", "Daria Miel"]
+      },
+      {
+        "ans": ["melón", "chica", "clásico", "verdes"],
+        "models": ["Clark Burbuja", "Seymour Onix", "Lucas Caoba", "Ramirez grafito"]
+      },
+      {
+        "ans": ["melón", "chica", "clásico", "rojos"],
+        "models": ["Clark Caoba", "Seymour Sombra", "Lucas Onix", "Ramirez Latte"]
+      },
+      {
+        "ans": ["melón", "chica", "moderno", "verdes"],
+        "models": ["Clark Burbuja", "Ramirez Latte", "Chavela Eucalipto", "Seymour Sombra"]
+      },
+      {
+        "ans": ["melón", "chica", "moderno", "rojos"],
+        "models": ["Clark Caoba", "Ramirez Caoba", "Chavela Chicle", "Seymour Onix"]
+      },
+      {
+        "ans": ["melón", "grande", "clásico", "verdes"],
+        "models": ["Clark Grafito", "Esquivel Eucalipto", "Martin Maple", "Janis Nogal"]
+      },
+      {
+        "ans": ["melón", "grande", "clásico", "rojos"],
+        "models": ["Clark Caoba", "Esquivel Niquel", "Martin Onix", "Janis Cósmico"]
+      },
+      {
+        "ans": ["melón", "grande", "moderno", "verdes"],
+        "models": ["Esquivel Eucalipto", "Janis Cosmico", "Martin Onix", "Woody Cedro"]
+      },
+      {
+        "ans": ["melón", "grande", "moderno", "rojos"],
+        "models": ["Esquivel Niquel", "Janis Cobalto", "Martin Maple B.", "Woody Grafito"]
+      },
+      {
+        "ans": ["calabaza", "chica", "clásico", "verdes"],
+        "models": ["Parks Plomo", "Aldous Grafito", "Ramirez Grafito", "Daria Jr. Onix"]
+      },
+      {
+        "ans": ["calabaza", "chica", "clásico", "rojos"],
+        "models": ["Parks Latón", "Aldous Nogal", "Ramirez Grafito", "Daría Jr. cobalto"]
+      },
+      {
+        "ans": ["calabaza", "chica", "moderno", "verdes"],
+        "models": ["Parks Cobre", "Chavela  Chicle", "Daria Jr. Musgo", "Aldous cereza"]
+      },
+      {
+        "ans": ["calabaza", "chica", "moderno", "rojos"],
+        "models": ["Parks Latón", "Chavela Eucalipto", "Daria Jr. Cobalto", "Aldous Burbuja"]
+      },
+      {
+        "ans": ["calabaza", "grande", "clásico", "verdes"],
+        "models": ["Daria Onix", "Moleman Sombra", "Aldous Grafito", "Gonzo Latón"]
+      },
+      {
+        "ans": ["calabaza", "grande", "clásico", "rojos"],
+        "models": ["Daria Burbuja", "Moleman Onix", "Aldous Nogal", "Gonzo Cobre"]
+      },
+      {
+        "ans": ["calabaza", "grande", "moderno", "verdes"],
+        "models": ["Gonzo Latón", "Moleman Sombra", "Aldous Burubuja", "Daria Cereza"]
+      },
+      {
+        "ans": ["calabaza", "grande", "moderno", "rojos"],
+        "models": ["Gonzo Latón", "Moleman Caoba", "Aldous Cereza", "Daria Miel"]
+      },
+      {
+        "ans": ["guanábana", "chica", "clásico", "verdes"],
+        "models": ["Ginsberg Sombra", "Barragán Maple", "Seymour ónix", "Ramirez Latte"]
+      },
+      {
+        "ans": ["guanábana", "chica", "clásico", "rojos"],
+        "models": ["Ginsberg Caoba", "Barragán Cósmico", "Seymour Grafito", "Ramirez grafito"]
+      },
+      {
+        "ans": ["guanábana", "chica", "moderno", "verdes"],
+        "models": ["Ginsberg Sombra", "Barragán Chicle", "Seymour ónix", "Ramirez Latte"]
+      },
+      {
+        "ans": ["guanábana", "chica", "moderno", "rojos"],
+        "models": ["Ginsberg Cereza", "Barragán Eucalipto", "Seymour Sombra", "Ramirez Caoba"]
+      },
+      {
+        "ans": ["guanábana", "grande", "clásico", "verdes"],
+        "models": ["Janis Nogal", "Moleman Onix", "Truman cedro matte", "Stanley Nogal"]
+      },
+      {
+        "ans": ["guanábana", "grande", "clásico", "rojos"],
+        "models": ["Janis Cósmico", "Moleman Caoba", "Truman Ónix", "Stanley Onix"]
+      },
+      {
+        "ans": ["guanábana", "grande", "moderno", "verdes"],
+        "models": ["Janis Niquel", "Moleman Sombra", "Truman cedro matte", "Gonzo Cobre"]
+      },
+      {
+        "ans": ["guanábana", "grande", "moderno", "rojos"],
+        "models": ["Janis Cósmico", "Moleman Caoba", "Truman Ónix", "Gonzo Latón"]
+      }
+    ]
+}
