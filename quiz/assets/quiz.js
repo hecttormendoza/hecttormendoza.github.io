52e92cd9c4e0802b0c92017f352612c9714c3567
Héctor Mendoza <hecttormendoza@gmail.com>
2017-09-01 17:00:30 -0500

    Back button error

--- quiz/assets/quiz.js
+++ quiz/assets/quiz.js
@@ -4,10 +4,10 @@ function searchAnswer () {
   for (var i = 0; i < dbQuiz[gender].length; i++) {
     // dbQuiz[gender][i].ans.length == answers.length && dbQuiz[gender][i].ans.every(function(v,i) {return v === answers[i]})
     if (JSON.stringify(dbQuiz[gender][i].ans.sort()) == JSON.stringify(answers.sort())) {
+      var container = document.getElementById("matches");
       console.log(dbQuiz[gender][i].models);
       container.innerHTML = "";
       for (var j = 0; j < dbQuiz[gender][i].models.length; j++) {
-        var container = document.getElementById("matches");
         container.innerHTML += '<div class="match"><img src="https://cdn.shopify.com/s/files/1/0838/5577/products/front_parks_cobre_large.jpg?v=1497995287" alt=""><p class="din">'+ dbQuiz[gender][i].models[j] +'</p><a href="#"><span class="apercu_bold">VER MODELO</span></a></div>';
       }
     }
