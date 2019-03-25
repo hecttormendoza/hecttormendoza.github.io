1c98c6b64fe2217332dc49e3f83d82e81bbf832d
Héctor Mendoza <hecttormendoza@gmail.com>
2019-03-25 16:12:01 -0600

    [DEL] Removed files not using

--- rep/app/mailers/application_mailer.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: 'info@repmexico.com', to: 'info@repmexico.com', subject: 'Contacto REP'
-  layout 'mailer'
-end
--- rep/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-
-  root 'static_pages#home'
-  post '/contact', to: 'static_pages#contact', as: 'contact'
-
-end
